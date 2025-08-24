b8738a5f382c6acb42ed8c655ec6f6968f8ca5ad
Banani Satapathy <bananisatapathy2004@gmail.com>
2025-08-24 22:27:44 +0530

    updated trending page

--- src/app/trends/trends.ts
+++ src/app/trends/trends.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {  AfterViewInit, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-trends',
@@ -7,6 +8,64 @@ import { Component } from '@angular/core';
   styleUrl: './trends.css'
 })
 export class Trends {
+  private currentIndex = 0;
+  private track!: HTMLElement;
+  private items!: NodeListOf<HTMLElement>;
+  private prevBtn!: HTMLElement;
+  private nextBtn!: HTMLElement;
 
+  constructor(private renderer: Renderer2) {}
 
+  ngAfterViewInit(): void {
+    this.track = document.getElementById('carouselTrack') as HTMLElement;
+    this.items = this.track.querySelectorAll('.carousel-item');
+    this.prevBtn = document.getElementById('prevBtn') as HTMLElement;
+    this.nextBtn = document.getElementById('nextBtn') as HTMLElement;
+
+    this.updateButtons();
+
+    this.renderer.listen(this.prevBtn, 'click', () => this.move(-1));
+    this.renderer.listen(this.nextBtn, 'click', () => this.move(1));
+  }
+
+  move(direction: number): void {
+    const visibleItems = this.getVisibleItems();
+    const maxIndex = this.items.length - visibleItems;
+
+    this.currentIndex += direction;
+
+    if (this.currentIndex < 0) this.currentIndex = 0;
+    if (this.currentIndex > maxIndex) this.currentIndex = maxIndex;
+
+    const itemWidth = this.items[0].offsetWidth + 20; // 20px gap
+    const translateX = -(this.currentIndex * itemWidth);
+
+    this.track.style.transform = `translateX(${translateX}px)`;
+    this.updateButtons();
+  }
+
+  getVisibleItems(): number {
+    const width = window.innerWidth;
+    if (width <= 600) return 2; // mobile
+    if (width <= 900) return 3; // tablet
+    if (width <= 1200) return 4; // small laptop
+    return 5; // desktop
+  }
+
+  updateButtons(): void {
+    const visibleItems = this.getVisibleItems();
+    const maxIndex = this.items.length - visibleItems;
+
+    if (this.currentIndex === 0) {
+      this.prevBtn.classList.add('nav-disabled');
+    } else {
+      this.prevBtn.classList.remove('nav-disabled');
+    }
+
+    if (this.currentIndex >= maxIndex) {
+      this.nextBtn.classList.add('nav-disabled');
+    } else {
+      this.nextBtn.classList.remove('nav-disabled');
+    }
+  }
 }
